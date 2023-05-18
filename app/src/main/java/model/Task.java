003bf019e120aaef84c1416e6602295fb234a5d1
Gustavo Melo <108017300+Gust4voMelo@users.noreply.github.com>
2023-05-18 20:51:25 -0300

    Subindo código

---
+++ app/src/main/java/model/Task.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author gusta
+ */
+public class Task {
+    
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean isCompleted;
+    private Date deadline;
+    private Date createdAt;
+    private Date updatedAt;
+}
