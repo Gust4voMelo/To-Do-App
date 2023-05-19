3871589ead32d6f99db79e0f0c8c9c7459df77bb
Gustavo Melo <108017300+Gust4voMelo@users.noreply.github.com>
2023-05-18 21:31:27 -0300

    Commit 2.0 18/05

---
+++ app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author gusta
+ */
+public class ConnectionFactory {
+  
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = ""
+    public static final String USER = "root";
+    public static final String PASS = "root";
+}
